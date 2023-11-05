401b98b22a6b19087ee2dd37f84e9ffbcef03692
Roman Yarygin <ryarygin@livegenic.com>
2023-11-05 21:24:19 +1000

    Add task functionality

--- src/script.js
+++ src/script.js
@@ -2,11 +2,32 @@ class Application {
 
   constructor() {
     this.tasks = [];
+
+    this.createTaskBtn = document.getElementById("create-task-button");
+    this.todoTasksList = document.getElementById("todo-tasks-list");
+    this.inProgressTasksList = document.getElementById("in-progress-tasks-list");
+    this.doneTasksList = document.getElementById("done-tasks-list");
   }
 
-  addTask(name) {  
-    let task = new Task(name);
+  run() {
+    this.createTaskBtn.addEventListener('click', () => this.addTask());
+  }
+
+  update() {
+    let tasksHTML = this.tasks.map((task) => task.render()).join("");
+    this.todoTasksList.innerHTML = tasksHTML;
+  }
+
+  addTask() {
+    let taskName = prompt("Введите название задачи");
+    if (!taskName) {
+      return;
+    }
+
+    let task = new Task(taskName);
     this.tasks.push(task);
+
+    this.update();
   }
 
   destroyTask(taskId) {
@@ -16,10 +37,19 @@ class Application {
 }
 
 class Task {
-  
+
   constructor(name) {
     this.name = name;
+    this.id = Date.now();
     this.isCompleted = false;
+  }
+
+  render() {
+    return `
+      <div class="task">
+        <p>${this.name}</p>
+      </div>
+    `;
   }
 
   edit() {
@@ -29,19 +59,18 @@ class Task {
   destroy() {
 
   }
-  
+
   complete() {
 
   }
 
   uncomplete() {
-      
+
   }
 
 }
 
 let application = new Application();
-application.addTask("Задача №1");
-application.addTask("Задача №2");
+application.run();
 
 
